3129034eace2de0202e6b184c0627ee3fb32fbd0
Ilya Katlinsky <ilya.katlinsky@gmail.com>
2015-03-01 22:18:18 +0300

    Комментарии для нейронной сети

--- nn/neuron.py
+++ nn/neuron.py
@@ -8,17 +8,19 @@ __author__ = 'ikatlinsky'
 
 class Neuron():
     """
-
+    Класс представляет собой обхект - нейрон. параметрами класса являются функция активации и список весов
     """
     weights = []
     act_func = lambda x: x
 
     def __init__(self, size, act_func):
         """
-
-        :param size:
-        :param act_func:
-        :return:
+        Создает объект - нейрон.ю исходя из входных параметров: длина списка весов, функция активации.
+        Изначальные значения весов задаются произвольно.
+        :param size: размер списка весов, должен быть больше 0
+        :param act_func: функция активации нейрона
+        :exception ValueError
+        :return: новый объект нейрон
         """
         if size < 1:
             raise ValueError("Length of neuron weights should be greater than 0")
@@ -28,36 +30,40 @@ class Neuron():
 
     def get_value(self, x):
         """
-
-        :param x:
-        :return:
+        Возвращает результат действия функции активации на нейрон при заданных значениях :param x
+        :param x: значения нейрона, при кторых необходимо получить результатю Длина списка знаяений
+        должна совпадать с длиной списка весов.
+        :exception ValueError
+        :return: результат действия функции активации
         """
         if len(x) != len(self.weights):
-            raise AssertionError("Length of neuron values and weights should be equal.", x, self.weights)
+            raise ValueError("Length of neuron values and weights should be equal.", x, self.weights)
 
         scalar = np.dot(x, self.weights)
         return self.act_func(scalar)
 
     def set_weight(self, weights):
         """
-
-        :param weights:
-        :return:
+        Задает новые значения для весов нейрона.
+        :param weights: список весов, длина списка должна совпадать с текущей длиной весов
+        :exception ValueError
+        :return: новые значния весов
         """
         if len(weights) != len(self.weights):
-            raise AssertionError("Length of neuron weights should be equal.", self.weights)
+            raise ValueError("Length of neuron weights should be equal.", self.weights)
 
         self.weights = weights
         return self.weights
 
     def add_to_weights(self, weights):
         """
-
-        :param weights:
-        :return:
+        Добавляет к текущим значениям весов поправку
+        :param weights: список весов поправки, длина списка должна совпадать с текущкй длиной весов
+        :exception ValueError
+        :return: новые значения весов
         """
         if len(weights) != len(self.weights):
-            raise AssertionError("Length of neuron weights should be equal.", self.weights)
+            raise ValueError("Length of neuron weights should be equal.", self.weights)
 
         self.weights = np.add(self.weights, weights)
         return self.weights
@@ -65,21 +71,26 @@ class Neuron():
 
 def test():
     """
-
+    Тестирования процесса создания нейрона, вычисления результата функции активации, замены весов и изменения весов
     :return:
     """
     n = Neuron(5, nu.linear_activation)
     print "Веса нейрона: %s" % n.weights
+    print "\n"
 
     x = nu.generate_random_list(5)
     print "Входной вектор нейрона: %s" % x
+    print "\n"
 
     print "Применяем функцию активации: %s" % n.get_value(x)
+    print "\n"
 
     w = nu.generate_random_list(5)
     print "Заменяем веса: %s" % n.set_weight(w)
+    print "\n"
 
     print "Изменяем веса: %s" % n.add_to_weights(x)
+    print "\n"
 
 test()
 
--- nn/neuron_layer.py
+++ nn/neuron_layer.py
@@ -1,23 +1,24 @@
 # coding=utf-8
 from neuron import Neuron
 import neuron_utils as nu
+import random as rnd
 
 __author__ = 'ikatlinsky'
 
 
 class NeuronLayer:
     """
-
+    Объект предстваляющий собой слой нейронов. Объединяет определенной количество нейронов в единое целое
     """
     neurons = []
 
     def __init__(self, layer_size, neuron_size, activation_function):
         """
-
-        :param layer_size:
-        :param neuron_size:
-        :param activation_function:
-        :return:
+        Создает слой нейронов исходя из заданных параметров
+        :param layer_size: размер слоя - количество нейронов в слое
+        :param neuron_size: длина параметров для нейронов - длина списка весов и значений
+        :param activation_function: функция активации, которая будет присвоена всем нейрона в слое
+        :return: новый объект слоя
         """
         if layer_size < 1:
             raise ValueError("Neuron layer size should be greater than 1")
@@ -29,13 +30,22 @@ class NeuronLayer:
 
     def get_size(self):
         """
+        Возвращает количество нейронов в слое
+        :return количество нейронов
         """
         return len(self.neurons)
 
+    def get_neuron_size(self):
+        """
+        Возвращает размер параметров нейронов
+        :return количество нейронов
+        """
+        return len(self.neurons[0].weights)
+
     def get_value(self, x_list):
         """
-
-        :param x_list:
+        Возращает результат функции активации для каждого нейрона в слое, исходя их списка значений для каждого нейрона
+        :param x_list: список значений для всех нейронов, длина списка должна совпадать с числов нейронов в слое
         """
         if len(x_list) != len(self.neurons):
             raise ValueError("Size of neuron values list should be equal to number of neurons")
@@ -44,19 +54,22 @@ class NeuronLayer:
 
     def set_weights(self, weights_list):
         """
-
-        :param weights_list:
+        Задает веса для всех нейронв исходя их входного списка
+        :param weights_list: новые значения весов для нейронов, длина должна совпадать с числом нейронов, каждый
+        подсписок должен совпадать по длине с длиной весов нейронов
+        :return новые веса для нейронов
         """
         if len(weights_list) != len(self.neurons):
             raise ValueError("Size of weights list should be equal to number of neurons")
 
-        [self.neurons[i].set_weight(weights_list[i]) for i in range(len(weights_list))]
+        return [self.neurons[i].set_weight(weights_list[i]) for i in range(len(weights_list))]
 
-    def set_weights(self, weights, i):
+    def set_weights_single(self, weights, i):
         """
-
-        :param weights:
-        :param i:
+        Залает веса для i-го нейрона
+        :param weights: список значений весов, длина должна совпдать с длиной весов нейрона
+        :param i: номер нейрона для присвоения новых весов, номер нейрона не должен быть больше количества весов
+        :return новые веса для нейрона
         """
         if i > len(self.neurons):
             raise ValueError("There are no such neuron in layer")
@@ -64,24 +77,42 @@ class NeuronLayer:
         if len(weights) != len(self.neurons[i].weights):
             raise ValueError("Length of weights lisy should be equal")
 
-        self.neurons[i].set_weight(weights)
+        return self.neurons[i].set_weight(weights)
 
 
 def test():
     """
-
-    :return:
+    Тестирование создани объекта слоя и проведение основныъ операций
     """
     # Создаем слой нейронов
     neuron_layer = NeuronLayer(5, 5, nu.linear_activation)
 
     # Выводим нейроны в слое и их длину
+    print "Выводим нейроны в слое и их длину:"
     print neuron_layer.neurons
     print len(neuron_layer.neurons)
+    print "\n"
 
-    # Выводим веся для каждого нейрона и их функцию активации
+    # Выводим веса для каждого нейрона и их функцию активации
+    print "Выводим веса для каждого нейрона и их функцию активации:"
     for neuron in neuron_layer.neurons:
         print "Neuron weights: %s and activation function: %s" % (neuron.weights, neuron.act_func)
+    print "\n"
+
+    # Результат действия функции активации
+    print "Результат действия функции активации:"
+    print neuron_layer.get_value([[rnd.random() for _ in range(neuron_layer.get_neuron_size())] for _ in range(neuron_layer.get_size())])
+    print "\n"
+
+    # Замена весов всех нейронов
+    print "Замена весов всех нейронов:"
+    print neuron_layer.set_weights([nu.generate_random_list(neuron_layer.get_neuron_size()) for _ in range(neuron_layer.get_size())])
+    print "\n"
+
+    # Замена весов для одного нейрона
+    print "Замена весов для одного нейрона:"
+    print neuron_layer.set_weights_single(nu.generate_random_list(neuron_layer.get_neuron_size()), 2)
+    print "\n"
 
 
 def test_exceptions():
